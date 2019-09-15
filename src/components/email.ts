17753999efecec1857c16c690456a8351f139148
bkawk <email.will.in.china@gmail.com>
2019-09-15 15:24:56 +0800

    windows production

--- src/components/email.ts
+++ src/components/email.ts
@@ -14,8 +14,6 @@ class Email {
 
   static confirmEmail = async (fullName: string, email: string, confirmationCode: string) => {
     try {
-
-
       const html = `
       <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html xmlns="http://www.w3.org/1999/xhtml">
