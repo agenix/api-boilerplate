b3fc6f8a68b898adc1075e4c576f6de9b2d6ca8e
bkawk <email.will.in.china@gmail.com>
2019-09-15 13:12:40 +0800

    reset email

--- src/components/email.ts
+++ src/components/email.ts
@@ -7,39 +7,76 @@ AWS.config.update({
 });
 
 class Email {
-    static confirmEmail = async (fullName: string, email: string, confirmationCode: string) => {
-   try {
-    const html = `
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-      <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <title>Confirm your Worbli account, ${fullName}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </head>
-      <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
-        Hi, ${fullName}.<br/>
-        Please confirm your Worbli account by clicking this link:
-        http://127.0.0.1:3000/confirm_email/${confirmationCode}/<br/>
-        Once you confirm, you will have full access to Worbli and all future<br/>
-        notifications will be sent to this email address.<br/><br/>
-        - Team Worbli<br/>
-      </body>
-    </html>
-    `;
-    const params = {
-      Destination: { ToAddresses: [email] },
-      Message: { Body: { Html: { Charset: 'UTF-8', Data: html }},
-      Subject: { Charset: 'UTF-8', Data: `Confirm your worbli account, ${fullName}`} },
-      ReplyToAddresses: ['do-not-reply@worbli.io'], Source: 'do-not-reply@worbli.io',
-    };
-    const data = await new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-    if (!data) return false; else return true;
-   } catch (err) {
-      // tslint:disable-next-line: no-console
-      console.log(err);
-   }
+  static confirmEmail = async (fullName: string, email: string, confirmationCode: string) => {
+    try {
+      const html = `
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+          <title>Confirm your Worbli account, ${fullName}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </head>
+        <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
+          <h1>Confirm your Worbli account<h1/>
+          <hr/>
+          Hi, ${fullName}.<br/>
+          Please confirm your Worbli account by clicking this link:
+          http://127.0.0.1:3000/confirm_email/${confirmationCode}/<br/>
+          Once you confirm, you will have full access to Worbli and all future<br/>
+          notifications will be sent to this email address.<br/><br/>
+          - Team Worbli<br/><br/>
+          Control which emails you receive from Worbli by adjusting your <a href='http://127.0.0.1:3000/my/settings/'>settings.<a/>
+        </body>
+      </html>
+      `;
+      const params = {
+        Destination: { ToAddresses: [email] },
+        Message: { Body: { Html: { Charset: 'UTF-8', Data: html }},
+        Subject: { Charset: 'UTF-8', Data: `Confirm your worbli account, ${fullName}`} },
+        ReplyToAddresses: ['do-not-reply@worbli.io'], Source: 'do-not-reply@worbli.io',
+      };
+      const data = await new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+      if (!data) return false; else return true;
+    } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.log(err);
+    }
+  };
 
+  static resetPassword = async (fullName: string, email: string, resetCode: string) => {
+    try {
+      const html = `
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+          <title>Reset your Worbli password, ${fullName}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </head>
+        <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
+          <h1>Reset your Worbli password<h1/>
+          <hr/>
+          Hi, ${fullName}.<br/>
+          If you requested a password reset link, click the link below. If you didn’t make the request, you can safely discard this email.
+          http://127.0.0.1:3000/reset_password/${resetCode}/<br/><br/>
+          - Team Worbli<br/><br/>
+          Control which emails you receive from Worbli by adjusting your <a href='http://127.0.0.1:3000/my/settings/'>settings.<a/>
+        </body>
+      </html>
+      `;
+      const params = {
+        Destination: { ToAddresses: [email] },
+        Message: { Body: { Html: { Charset: 'UTF-8', Data: html }},
+        Subject: { Charset: 'UTF-8', Data: `reset your Worbli password`} },
+        ReplyToAddresses: ['do-not-reply@worbli.io'], Source: 'do-not-reply@worbli.io',
+      };
+      const data = await new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+      if (!data) return false; else return true;
+    } catch (err) {
+        // tslint:disable-next-line: no-console
+        console.log(err);
+    }
   };
 }
 
